995ca86a370d0cea21c9eaa890ccd00007ea67e1
Sean Gillies <sean@mapbox.com>
2020-10-20 18:23:22 -0600

    Warn about future python support (#33)

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4",
     install_requires=["click >= 4.0, < 8"],
     extras_require={"test": ["pytest-cov"],},
 )
